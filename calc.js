efb5e7984b818d854be02790a3d65f97af119941
stxss <93729036+stxss@users.noreply.github.com>
2023-01-05 17:25:13 +0000

    Add operate function

--- calc.js
+++ calc.js
@@ -25,9 +25,30 @@ function divide(num1, num2) {
     return product;
 }
 
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        add(num1, num2);
+    } else if (operator === "-") {
+        subtract(num1, num2);
+    } else if (operator === "*") {
+        multiply(num1, num2);
+    } else if (operator === "/") {
+        divide(num1, num2);
+    } else {
+        console.log("Invalid Operator!");
+    }
+}
+
+
 
 add(2,3);
 subtract(5,2);
 multiply(4,3);
 divide(1,3);
 divide(4,2);
+console.log("-------")
+operate("+", 5, 5);
+operate("-", 5, 5);
+operate("*", 5, 5);
+operate("/", 5, 5);
+operate("#", 5, 5);
