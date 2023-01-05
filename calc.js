1b49e4d06cb64a965d895495db9ca27c4191000e
stxss <93729036+stxss@users.noreply.github.com>
2023-01-05 17:20:51 +0000

    Add basic operations

---
+++ calc.js
@@ -0,0 +1,33 @@
+// Basic functions
+
+function add(num1, num2) {
+    let sum = num1 + num2;
+    console.log(sum);
+    return sum;
+}
+
+
+function subtract(num1, num2) {
+    let sum = num1 - num2;
+    console.log(sum);
+    return sum;
+}
+
+function multiply(num1, num2) {
+    let product = num1 * num2;
+    console.log(product);
+    return product;
+}
+
+function divide(num1, num2) {
+    let product = (num1 / num2);
+    console.log(product);
+    return product;
+}
+
+
+add(2,3);
+subtract(5,2);
+multiply(4,3);
+divide(1,3);
+divide(4,2);
