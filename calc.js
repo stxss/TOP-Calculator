fd1a12119cde545eb55c24a0b22e0954ea416b64
stxss <93729036+stxss@users.noreply.github.com>
2023-01-08 16:39:21 +0000

    Represent correctly the operations, allowing for multiple consequent operations without having to press 'equals' every time

--- calc.js
+++ calc.js
@@ -11,6 +11,7 @@ let subBtn = document.querySelector(".subtract");
 let sumBth = document.querySelector(".sum");
 let eqlBtn = document.querySelector(".equals");
 let dotBtn = document.querySelector(".dot");
+let clrBtn = document.querySelector(".clear");
 
 // Setting the flags of each operator button
 let divBtnFlag = false; 
@@ -35,6 +36,10 @@ let slOperator = "";
 //  Passing the number values to numbers
 let n1;
 let n2;
+let result;
+
+// Flag for a new operation after the first
+let newOperation = false; 
 
 // Adding a listener for each of the number buttons, that will update the display when clicked.
 buttons.forEach((buttons) => {
@@ -54,6 +59,13 @@ buttons.forEach((buttons) => {
             n1 = parseInt(displayValue);
 
         } else {
+            if (displayValue && display.classList.contains("result")) {
+                display.classList.remove("result");
+                console.log(`new n1 is ${result}`);
+                n1 = result;
+                secValue = undefined;
+            }
+
             if (!eqlBtnFlag) {
                 if (!secValue) {
                     secValue = buttons.id;
@@ -66,74 +78,23 @@ buttons.forEach((buttons) => {
     
                 n2 = parseInt(secValue);
             } else {
-                console.log(operate(slOperator, displayValue, secValue))
-                display.textContent = operate(slOperator, displayValue, secValue);
                 display.classList.add("result");
             }
         }
-
-        // // Is the operator flag true?
-        // // If it is false
-        // if (!isOperator) {
-        //     // write to n1
-        // } else {
-        //     // write to n2
-        // }
-
-        // // Is n1 true? (aka does it have a number?)
-        // // If it is false
-        // if (!n1) {
-        //     // write to n1
-        // } else {
-        //     // n1 is true, but is the operator flag true?
-        //     // If it is false
-        //     if (!isOperator) {
-        //         // write to n1
-        //     } else {
-        //         // write to n2
-        //     }
-        // }
-
-        // // Is n2 true? (aka does it have a number?)
-        // if (!n2) {
-        //     // n2 is false, but is the operator flag true?
-        //     // If it is false
-        //     if (!isOperator) {
-        //         // write to n1
-        //     } else {
-        //         // write to n2
-        //     }
-        // } else {
-        //     // n2 is true, but is the equal button flag true?
-        //     // If it is false
-        //     if (!eqlBtnFlag) {
-        //         // Continue writing to n2
-        //     } else {
-        //         // output the number
-        //     }
-        // }
-
-
-        if ((!isOperator) || (!n1) || (!n2)) {
-            console.log(isOperator);
-            console.log(n1);
-            console.log(n2);
-        } else if ((isOperator) && (n1) && ((n2) && (eqlBtnFlag))){
-            operate(slOperator, displayValue, secValue);
-            display.classList.add("result");
-            isOperator = !isOperator;
-            n1 = undefined;
-            n2 = undefined;
-        }
     });
 });
-
 
 divBtn.addEventListener("click", () => {
     isOperator = true;
     slOperator = "/";
 
-    divBtnFlag = !divBtnFlag;
+    if (!newOperation) {
+        divBtnFlag = true;
+        newOperation = true;
+    } else {
+        divBtnFlag = true;
+        eqlBtn.click();
+    }
 
     console.log(`divider operator is ${divBtnFlag}`);
 });
@@ -142,7 +103,13 @@ mltBtn.addEventListener("click", () => {
     isOperator = true;
     slOperator = "*";
 
-    mltBtnFlag = !mltBtnFlag;
+    if (!newOperation) {
+        mltBtnFlag = true;
+        newOperation = true;
+    } else {
+        mltBtnFlag = true;
+        eqlBtn.click();
+    }
 
     console.log(`multiplication operator is ${mltBtnFlag}`);
 });
@@ -151,7 +118,13 @@ subBtn.addEventListener("click", () => {
     isOperator = true;
     slOperator = "-";
 
-    subBtnFlag = !subBtnFlag;
+    if (!newOperation) {
+        subBtnFlag = true;
+        newOperation = true;
+    } else {
+        subBtnFlag = true;
+        eqlBtn.click();
+    }
 
     console.log(`sub operator is ${subBtnFlag}`);
 });
@@ -160,7 +133,13 @@ sumBth.addEventListener("click", () => {
     isOperator = true;
     slOperator = "+";
 
-    sumBtnFlag = !sumBtnFlag;
+    if (!newOperation) {
+        sumBtnFlag = true;
+        newOperation = true;
+    } else {
+        sumBtnFlag = true;
+        eqlBtn.click();
+    }
 
     console.log(`sum operator is ${sumBtnFlag}`);
 });
@@ -171,8 +150,29 @@ dotBtn.addEventListener("click", () => {
 });
 
 eqlBtn.addEventListener("click", () => {
+    isOperator = true;
     eqlBtnFlag = !eqlBtnFlag;
     console.log(`equals is ${eqlBtnFlag}`);
+    operate(slOperator, n1, n2);
+
+});
+
+clrBtn.addEventListener("click", () => {
+    divBtnFlag = false;
+    mltBtnFlag = false;
+    subBtnFlag = false;
+    sumBtnFlag = false;
+    eqlBtnFlag = false;
+    dotBtnFlag = false;
+    displayValue = 0;
+    secValue = 0;
+    isOperator = false;
+    slOperator = "";
+    n1 = 0;
+    n2 = 0;
+    result = 0;
+    newOperation = false;
+    display.textContent = 0;
 });
 
 
@@ -186,7 +186,6 @@ function add(num1, num2) {
 
         let sum = (num1 + num2);
 
-        console.log(sum);
         sumBtnFlag = !sumBtnFlag;
         display.textContent = sum;
         return sum;
@@ -198,7 +197,6 @@ function subtract(num1, num2) {
     if ((n1) && (n2)) {
         let sum = (num1 - num2);
 
-        console.log(sum);
         subBtnFlag = !subBtnFlag;
 
         return sum;
@@ -210,7 +208,6 @@ function multiply(num1, num2) {
     if ((n1) && (n2)) {
         let product = (num1 * num2);
 
-        console.log(product);
         mltBtnFlag = !mltBtnFlag;
 
         return product;
@@ -222,27 +219,41 @@ function divide(num1, num2) {
     if ((n1) && (n2)) {
         let product = (num1 / num2);
 
-        console.log(product);
         divBtnFlag = !divBtnFlag;
-
+        result = product;
         return product;
     }
 }
 
 // Operate function, which is called to determine what operation to use, and the respective numbers that will be used in the operation
 function operate(operator, num1, num2) {
+    if ((!isOperator) || (!n1) || (!n2)) {
+        console.log(isOperator);
+        console.log(n1);
+        console.log(n2);
+    } else if ((isOperator) && (n1) && ((n2) && (eqlBtnFlag))){
 
-    if (operator === "+") {
-        add(num1, num2);
-    } else if (operator === "-") {
-        subtract(num1, num2);
-    } else if (operator === "*") {
-        multiply(num1, num2);
-    } else if (operator === "/") {
-        divide(num1, num2);
-    } else {
-        return "Invalid Operator!";
+        if (operator === "+") {
+            result = add(num1, num2);
+        } else if (operator === "-") {
+            result = subtract(num1, num2);
+        } else if (operator === "*") {
+            result = multiply(num1, num2);
+        } else if (operator === "/") {
+            result = divide(num1, num2);
+        } else {
+            return "Invalid Operator!";
+        }
+        console.log(result);
+        display.textContent = result;
+
+        display.classList.add("result");
+
+        n1 = result;
+        eqlBtnFlag = !eqlBtnFlag;
+        return result;
     }
+
 }
 
 
