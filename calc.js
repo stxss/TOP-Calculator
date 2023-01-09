06d500ba4a2464aaaa94e5a5d7fe38a2105cf26b
stxss <93729036+stxss@users.noreply.github.com>
2023-01-09 17:08:17 +0000

    Update the display immediately when clicking on a new operator, after already having one locked in, thus preventing an unwanted change of operators midway

--- calc.js
+++ calc.js
@@ -68,15 +68,24 @@ buttons.forEach((buttons) => {
 
 
 function handleOperatorClick(operator) {
+
+
+    if ((isOperator) && (n1) && (n2)) {
+        eqlBtn.click();
+    }
     isOperator = true;
     slOperator = operator;
     secValue = 0;
-    // Explicitly saying that the button was already pressed one time, preventing the further clicks of the button to trigger calculations, so the user has the limitation to only click the operator buttons one time per operation, preventing "spamming" the buttons. This strategy repeats for every operation
-    if (manyPressed < 1) {
-        // Proccing (aka triggering) the equal button to show the result when the user clicks an operator button when a result is already displayed, allowing for continuos calculations. This strategy repeats for every operation
-        eqlBtn.click();
-    }
+    manyPressed++;
+
+    // // Explicitly saying that the button was already pressed one time, preventing the further clicks of the button to trigger calculations, so the user has the limitation to only click the operator buttons one time per operation, preventing "spamming" the buttons. This strategy repeats for every operation
+    // if (manyPressed > 1) {
+    // // Proccing (aka triggering) the equal button to show the result when the user clicks an operator button when a result is already displayed, allowing for continuos calculations. This strategy repeats for every operation
+    //     eqlBtn.click();
+    // }
 }
+
+// }
 
 // Listener for the division button
 divBtn.addEventListener("click", () => {
@@ -116,9 +125,16 @@ eqlBtn.addEventListener("click", () => {
         result = 0;
     } else if (n2) {
         operate(slOperator, n1, n2);
-        manyPressed++; 
-    } 
-
+        // operate(slOperator, n1, n2);
+        console.log(n1);
+        console.log(slOperator);
+        console.log(n2);
+        console.log(result);
+        console.log(`manyPressed is ${manyPressed}`);
+        // manyPressed++;
+        // manyPressed = 0;
+        n1 = result;
+    }
 });
 
 // Clear button listener, clears all inputs and variables, resetting the calculator
@@ -130,6 +146,7 @@ clrBtn.addEventListener("click", () => {
     n1 = 0;
     n2 = 0;
     result = 0;
+    manyPressed = 0;
 });
 
 
@@ -156,14 +173,15 @@ function operate(operator, num1, num2) {
 
         // Setting the new num1 to the result, in case the user wants to do more shortened calculations and resetting the manyPressed flag/counter
         n1 = result;
-        manyPressed = 0;
         isOperator = false;
+        // manyPressed = 0;
     }
 }
 
-
+// Todo: take care of when pressing a different operator, for it to 
 // Todo: take care of pressing equals before entering all the numbers or symbols;
 // Todo: Add floating point numbers (e.g: 2.5 or 31.75, etc...), not allowing for an input of more than one dot, for example by disabling the dot button if there is already one on the display;
 // Todo: add a backspace/delete button, so the user can delete a wrong number;
 // Todo: add keyboard support;
 // Todo: make it look nice;
+// ! take care of when doing any operation and instead of clicking =, clicking any other operator, it concatenates the numbers instead of operating them
